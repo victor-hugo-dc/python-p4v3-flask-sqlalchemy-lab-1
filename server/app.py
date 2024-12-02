2ef084ec3ad1a0d321081a9e9781d8cfb722f9d5
Victor Hugo <delcarpio.vh@gmail.com>
2024-12-01 23:22:31 -0500

    feat: complete

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,7 +20,27 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_id(id):
+    earthquake = db.session.get(Earthquake, id)
+    if earthquake is None:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+    else:
+        return jsonify({
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }), 200
+        
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    response = {
+        "count": len(earthquakes),
+        "quakes": [{"id": eq.id, "location": eq.location, "magnitude": eq.magnitude, "year": eq.year} for eq in earthquakes]
+    }
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,9 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, Integer, Float, String, MetaData
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = "earthquakes"
+    id = db.Column(Integer, primary_key=True)
+    magnitude = db.Column(Float)
+    location = db.Column(String)
+    year = db.Column(Integer)
+    def __repr__(self):
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
